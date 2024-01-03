7d70b2c8f54aefa3153384de78b9ee68233d3fe1
Swastik Dash <swastikdash@Swastiks-MacBook-Pro.local>
2024-01-03 20:38:05 +0530

    Initial commit

---
+++ src/main/java/speer/assesment/note_it/NoteItApplication.java
@@ -0,0 +1,13 @@
+package speer.assesment.note_it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/speer/assesment/note_it/NoteItApplicationTests.java
@@ -0,0 +1,13 @@
+package speer.assesment.note_it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
